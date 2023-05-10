a7cc694693b09477285569b80d22075b19d203eb
mankeylol <mankeyooooo@gmail.com>
2023-05-10 14:31:20 +0530

    optimised for mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <>
     <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </Head>
       <Navbar />
       <div className="hero-section">
